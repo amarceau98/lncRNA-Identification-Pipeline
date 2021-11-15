671244ca8d9d72fac5d2b568d18289333b01c677
amarceau98 <88853090+amarceau98@users.noreply.github.com>
2021-11-15 10:50:44 -0500

    Create get_intergenicGTF.pl

---
+++ get_intergenicGTF.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/perl -w
+# get_intergenic.pl --- Get intergenic transcript information
+# Author: Fei Zhan <fei@fei-laptop>
+# Created: 04 Dec 2012
+# Modified by: Alexis Marceau
+# Modified: 29 Oct 2021
+# Being used in RNAseq to lncRNA pipeline development
+# Version: 0.01
+
+use warnings;
+use strict;
+
+print "Condition name, please be consistent with previous answers:\n";
+$Condition_name = <>;
+chomp($Condition_name);
+
+my $loci = '$lncRNA_Identification/CuffCompare/Condition_name_list_intergenic.txt';
+my $gtf = 'lncRNA_Identification/Stringtie/$Condition_name.ref.gtf';
+my $fileout = '$lncRNA_Identification/CuffCompare/$Condition_name.intergenic_loci.gtf';
+
+foreach my $i (0 .. scalar(@ARGV)-1) {
+  if($ARGV[$i] eq '-g') {
+    $gtf = $ARGV[++$i];
+  }elsif($ARGV[$i] eq '-l') {
+    $loci = $ARGV[++$i];
+  }elsif($ARGV[$i] eq '-o') {
+    $fileout = $ARGV[++$i];
+  }
+}
+if($loci eq '' | $gtf eq '' | $fileout eq '') {
+  die "Usage:\n\t perl get_intergenic.pl -g [gtf] -l [loci] -o [output]\n\n";
+}
+
+open(LOCI,$loci) or die "Cannot open $loci.\n\nUsage:\n\t perl get_intergenic.pl -g [gtf] -l [loci] -o [output]\n\n";
+open(GTF,$gtf) or die "Cannot open $gtf.\n\nUsage:\n\t perl get_intergenic.pl -g [gtf] -l [loci] -o [output]\n";
+open(OUT,'>',$fileout) or die "Cannot open $fileout.\n\nUsage:\n\t perl get_intergenic.pl -g [gtf] -l [loci] -o [output]\n";
+
+my %hash_loci;
+while(<LOCI>) {
+  chomp;
+  $hash_loci{$_}++;
+}
+close LOCI;
+
+while(<GTF>) {
+  chomp;
+  my($attributes) = (split("\t",$_))[8];
+  if($attributes =~ m/transcript_id "(.+?)";/) {
+    my $loci = $1;
+    if(exists $hash_loci{$loci}) {
+      print OUT $_,"\n";
+    }
+  }
+}
+close GTF;
+close OUT;
+
+
+
+
+
+
+
+
+
+
+
+__END__
+
+=head1 NAME
+
+get_intergenic.pl - Describe the usage of script briefly
+
+=head1 SYNOPSIS
+
+get_intergenic.pl [options] args
+
+      -opt --long      Option description
+
+=head1 DESCRIPTION
+
+Stub documentation for get_intergenic.pl, 
+
+=head1 AUTHOR
+
+Fei Zhan, E<lt>fei@fei-laptopE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Fei Zhan
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 BUGS
+
+None reported... yet.
+
+=cut
